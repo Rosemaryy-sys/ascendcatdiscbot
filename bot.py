804c1f0f1c3e49115b80c0fc17d0e553df9866b4
ASCENDCAT STUDIOS <86455540+Rosemaryy-sys@users.noreply.github.com>
2021-09-18 19:35:41 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import random
 from discord.ext import commands
 
 
-client = commands.Bot(command_prefix="-", case_insensitive=True)
+client = commands.Bot(command_prefix="a!", case_insensitive=True)
 
 
 
@@ -42,13 +42,52 @@ async def history(ctx):
 async def mmm(ctx):
 	await ctx.send("u having a stroke?")
 
+@client.command()
+async def math(ctx):
+	await ctx.send("i am not smart at math")
+		
+@client.command()
+async def faggot(ctx):
+	await ctx.send("huh? what u say?")
+
+@client.command()
+async def fruity(ctx):
+	await ctx.send("not me, its you.")
+
+@client.command()
+async def ascend(ctx):
+	await ctx.send("ascended into the deeplands")
+
+@client.command()
+async def pineapple(ctx):
+	await ctx.send("pineapple, pen, apple.")
+
+@client.command()
+async def kaeyaballs(ctx):
+	await ctx.send("police, this is the person.")
+
+@client.command()
+async def kaeyaballcheese(ctx):
+	await ctx.send("sir come with me your goin to hatsune miku prison")
+
+@client.command()
+async def vampirecookie(ctx):
+	await ctx.send("thinking bout that man always ")
+
+@client.command()
+async def dont(ctx):
+	await ctx.send("dont shue me i need a family to raise :cryingemo:")
+
+@client.command()
+async def floppa(ctx):
+	await ctx.send("No flop no flopsta, just flpppaaa. Flopaa no flop no flopsta.")
 
 
 
 
-# still make command_prefix = "-"
-
-#Commands = 5 as of "CURRENT" 
-#CURRENTLY = 5 Commands
 
 
+# still make command_prefix = "a!"
+
+#Commands = 14 as of "CURRENT" 
+#CURRENTLY = 14 Commands
